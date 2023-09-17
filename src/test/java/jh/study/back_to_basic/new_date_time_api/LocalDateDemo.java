ae035762987491da770f7d3d10cc9ed59468adac
jh <purepleya@gmail.com>
2023-09-17 23:07:27 +0900

    new date api 관련 작성 중

---
+++ src/test/java/jh/study/back_to_basic/new_date_time_api/LocalDateDemo.java
@@ -0,0 +1,13 @@
+package jh.study.back_to_basic.new_date_time_api;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+public class LocalDateDemo {
+
+    @Test
+    void defaultDemo() {
+        LocalDate localDate = LocalDate.now();
+    }
+}
